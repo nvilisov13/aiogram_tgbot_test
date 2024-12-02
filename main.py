7aad9b3f7064ab8b1e35cee4f439c352dff0b833
Вилисов Николай <asup_u03@slavyanka.by>
2024-12-02 16:18:35 +0300

    Created - 02.12.2024 16:18

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, Router
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
 from aiogram.fsm.state import State, StatesGroup
-from aiogram.types import Message, User
+from aiogram.types import Message, User, CallbackQuery
 from aiogram_dialog import Dialog, DialogManager, StartMode, Window, setup_dialogs
-from aiogram_dialog.widgets.text import Format
+from aiogram_dialog.widgets.text import Format, List, Multi, Const, Case
+from aiogram_dialog.widgets.kbd import Button
 from environs import Env
 
 env = Env()
@@ -11,33 +14,53 @@ env.read_env()
 
 BOT_TOKEN = env('BOT_TOKEN')
 
-bot = Bot(token=BOT_TOKEN)
+bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 dp = Dispatcher()
+
+router = Router()
 
 
 class StartSG(StatesGroup):
     start = State()
 
 
-async def username_getter(dialog_manager: DialogManager, event_from_user: User, **kwargs):
-    return {'username': event_from_user.username}
+# Это хэндлер, обрабатывающий нажатие инлайн-кнопок
+async def button_clicked(callback: CallbackQuery, button: Button, dialog_manager: DialogManager):
+    another_button = dialog_manager.dialog_data.get('another_button')
+    dialog_manager.dialog_data.update(another_button=not another_button)
+
+
+# Это геттер
+async def get_button_status(dialog_manager: DialogManager, **kwargs):
+    another_button = dialog_manager.dialog_data.get('another_button')
+    return {'button_status': another_button}
 
 
 start_dialog = Dialog(
     Window(
-        Format('Привет, {username}!'),
-        getter=username_getter,
-        state=StartSG.start
+        Const('На кнопки из этого сообщения можно нажать!'),
+        Button(
+            text=Const('Нажми меня!'),
+            id='button_1',
+            on_click=button_clicked),
+        Button(
+            text=Const('И меня нажми!'),
+            id='button_2',
+            on_click=button_clicked,
+            when='button_status'),
+        state=StartSG.start,
+        getter=get_button_status,
     ),
 )
 
 
+# Это классический хэндлер на команду /start
 @dp.message(CommandStart())
 async def command_start_process(message: Message, dialog_manager: DialogManager):
     await dialog_manager.start(state=StartSG.start, mode=StartMode.RESET_STACK)
 
 
-if __name__ == '__main__':
-    dp.include_router(start_dialog)
-    setup_dialogs(dp)
-    dp.run_polling(bot)
+dp.include_router(router)
+dp.include_router(start_dialog)
+setup_dialogs(dp)
+dp.run_polling(bot)
